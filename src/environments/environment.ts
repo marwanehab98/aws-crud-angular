2a0103f49d3abaf06283326652806206bc72ccc0
Marwan Ehab <marwan.el.sheikh14@gmail.com>
2023-12-22 15:33:13 +0200

    prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   COGNITO_USER_POOL_ID: 'us-west-1_ICpudhq1m',
   COGNITO_DOMAIN: 'https://oyounmasrdevtask.auth.us-west-1.amazoncognito.com',
   COGNITO_CLIENT_ID: '7ask39ktf3ptv0k2tfjvhje0l8',
-  REDIRECT_URI: 'http://localhost:4200',
+  REDIRECT_URI: 'https://aws-crud-angular.vercel.app',
   API_URL: 'https://6vkqtfzlw1.execute-api.us-west-1.amazonaws.com/test/items/'
 };
