d98e98b08195803d716a7c16aebe9e841d511915
Marwan Ehab <marwan.el.sheikh14@gmail.com>
2023-12-22 15:28:55 +0200

    ign

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  COGNITO_USER_POOL_ID: 'us-west-1_ICpudhq1m',
+  COGNITO_DOMAIN: 'https://oyounmasrdevtask.auth.us-west-1.amazoncognito.com',
+  COGNITO_CLIENT_ID: '7ask39ktf3ptv0k2tfjvhje0l8',
+  REDIRECT_URI: 'http://localhost:4200',
+  API_URL: 'https://6vkqtfzlw1.execute-api.us-west-1.amazonaws.com/test/items/'
+};
